14b1e7e5541d51b5f0bbec739530beda6bcae185
Nicklas Schioldan Winther <nick3845@edu.campusvejle.dk>
2020-01-21 11:33:11 +0100

    Add project files.

---
+++ WpfAppIntroduktion/BIZ/ClassBiz.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfAppIntroduktion.BIZ
+{
+    class ClassBiz
+    {
+        public ClassBiz()
+        {
+        }
+        Random random = new Random();
+
+        private List<int> GetRandomNumbers()
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                numbers.Add(random.Next(100_000, 1_000_001));
+            }
+            return numbers;
+        }
+
+        //Opgave 2
+        public void Number4711(ListBox listBox)
+        {
+            for (int i = 4711; i <= 4736; i++)
+            {
+                listBox.Items.Add(i.ToString());
+            }
+        }
+
+        //Opgave 3
+        public void RandomNumbers(ListBox listBox)
+        {
+            List<int> numbers = GetRandomNumbers();
+            foreach (int number in numbers)
+            {
+                listBox.Items.Add(number.ToString());
+            }
+            
+        }
+
+        //Opgave 4
+        public void RandomSortedNumbers(ListBox listBox)
+        {
+            List<int> numbers = GetRandomNumbers();
+
+            numbers.Sort();
+
+            foreach (int number in numbers)
+            {
+                listBox.Items.Add(number.ToString());
+            }
+        }
+
+        //Opgave 5
+        public void RandomNumbersSortedAndUnsorted(ListBox listBox)
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomNumber = random.Next(100_000, 1_000_001);
+                list1.Add(randomNumber);
+                list2.Add(randomNumber);
+            }
+            list2.Sort();
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                listBox.Items.Add($"{list1[i]} - {list2[i]}");
+            }
+        }
+
+        //Opgave 6
+        public void GetAverage(ListBox listBox)
+        {
+            List<int> numbers = GetRandomNumbers();
+            int average = FindAverage(numbers);
+
+            foreach (int number in numbers)
+            {
+                listBox.Items.Add(number);
+            }
+
+            listBox.Items.Add("");
+            listBox.Items.Add("Gennemsnit: " + average);
+        }
+        private int FindAverage(List<int> numbers)
+        {
+            int average = (int)numbers.Average();
+            return average;
+        }
+
+        //Opgave 7
+        public void GetAverageAndSubtract(ListBox listBox)
+        {
+            List<int> numbers = GetRandomNumbers();
+            numbers.Sort();
+            int average = FindAverage(numbers);
+            
+            foreach (int number in numbers)
+            {
+                listBox.Items.Add($"{number} - {average} = {number - average}");
+            }
+        }
+
+        //Opgave 8
+        public List<String> 
+
+    }
+}
---
+++ WpfAppIntroduktion/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppIntroduktion.BIZ;
+
+namespace WpfAppIntroduktion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBiz classBiz = new ClassBiz();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpg12_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.Items.Clear();
+            classBiz.Number4711(listBoxRes);
+        }
+
+        private void buttonOpg13_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.Items.Clear();
+            classBiz.RandomNumbers(listBoxRes);
+        }
+
+        private void buttonOpg14_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.Items.Clear();
+            classBiz.RandomSortedNumbers(listBoxRes);
+        }
+
+        private void buttonOpg15_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.Items.Clear();
+            classBiz.RandomNumbersSortedAndUnsorted(listBoxRes);
+        }
+
+        private void buttonOpg16_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.Items.Clear();
+            classBiz.GetAverage(listBoxRes);
+        }
+
+        private void buttonOpg17_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.Items.Clear();
+            classBiz.GetAverageAndSubtract(listBoxRes);
+        }
+    }
+}
